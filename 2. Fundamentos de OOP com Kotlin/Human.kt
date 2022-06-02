730036728d3201bdd720307dcef82201f6233b00
0xEDU <bfwaend@gmail.com>
2022-06-02 15:45:45 -0300

    added folder and created a class

---
+++ 2. Fundamentos de OOP com Kotlin/Human.kt
@@ -0,0 +1,15 @@
+// Creating a class
+
+// Class defining a human
+class Human{
+	var name:String = "Edu"
+	var cpf:String = "123.132.123-12"	
+}
+
+// Main function
+fun main() {
+	val edu = Human()
+
+	println(edu.name)
+	println(edu.cpf)
+}
