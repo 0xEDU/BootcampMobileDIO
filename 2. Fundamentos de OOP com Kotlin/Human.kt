78f1df515f08a3d82490686d9991a96524b71d72
0xEDU <bfwaend@gmail.com>
2022-06-05 14:12:43 -0300

    update to apply the private modifiers to 'set' function at cpf property

--- 2. Fundamentos de OOP com Kotlin/Human.kt
+++ 2. Fundamentos de OOP com Kotlin/Human.kt
@@ -1,9 +1,13 @@
-// Creating a class
+// 1. Creating a class
+// 2. 
 
 // Class defining a human
 class Human{
 	var name:String = "Edu"
+	
 	var cpf:String = "123.132.123-12"	
+	private set
+
 }
 
 // Main function
