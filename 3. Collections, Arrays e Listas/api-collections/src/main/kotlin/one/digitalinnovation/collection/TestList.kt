b92f5b00da0b73a00d1271810e26cd4a418fa5e2
0xEDU <bfwaend@gmail.com>
2022-06-13 14:03:32 -0300

    added sortedBy and groupBy to list test file

--- 3. Collections, Arrays e Listas/api-collections/src/main/kotlin/one/digitalinnovation/collection/TestList.kt
+++ 3. Collections, Arrays e Listas/api-collections/src/main/kotlin/one/digitalinnovation/collection/TestList.kt
@@ -1,18 +1,29 @@
 package one.digitalinnovation.collection
 
 fun main() {
-    val john = employee("john", 2000.00)
-    val joe = employee("joe", 3000.00)
-    val ana= employee("ana", 7000.00)
+    val john = employee("john", 10000.00, "CLT")
+    val joe = employee("joe", 3000.00, "PJ")
+    val ana= employee("ana", 7200.00, "CLT")
 
     val employees = listOf(john, joe, ana)
 
     employees.forEach { println(it) }
+
+    println("----------------------")
+    employees
+        .sortedBy { it.wage }
+        .forEach { println(it) }
+
+    println("----------------------")
+    employees
+        .groupBy { it.type }
+        .forEach {println(it)}
 }
 
 data class employee(
     val name: String,
-    val wage: Double
+    val wage: Double,
+    val type: String
 ) {
     override fun toString(): String =
         """
