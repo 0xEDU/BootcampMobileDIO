e85a0140066823d1346b7e6bd3d0599601d940b5
0xEDU <bfwaend@gmail.com>
2022-06-13 13:27:01 -0300

    created operations test file

---
+++ 3. Collections, Arrays e Listas/api-collections/src/main/kotlin/one/digitalinnovation/collection/TestOperations.kt
@@ -0,0 +1,27 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val wages = doubleArrayOf(1000.0, 7050.0, 2350.46)
+
+    for (wage in wages) {
+        println(wage)
+    }
+    println("---------------------")
+/*    println("Maior salário: ${wages.max()}")
+    println("Menor salário: ${wages.min()}")*/
+    println("Média salarial: ${wages.average()}")
+
+    val great = wages.filter { it > 2500 }
+    great.forEach{println(it)}
+
+    println("---------------------")
+    println(wages.count{it in 1000.0..8000.00 })
+
+    println("---------------------")
+    println(wages.find {it == 1000.0})
+    println(wages.find {it == 7050.0})
+
+    println("---------------------")
+    println(wages.any {it == 1000.0})
+    println(wages.any {it == 2350.46})
+}
